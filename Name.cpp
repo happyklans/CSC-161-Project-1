e7761439045760b0f98a83f080b88f7f041a4c6b
happyklans <bstanton4@student.cccs.edu>
2017-09-16 12:44:33 -0600

    finished the name class

--- Name.cpp
+++ Name.cpp
@@ -15,6 +15,29 @@ Name::Name(std::string fname, std::string lname)
 	Name::last_name = lname;
 }
 
+// gets user input for first and last name
+void Name::set_full_name()
+{
+	std::cout << "Enter your first name: ";
+	
+	std::cin >> first_name;
+	
+	if (std::cin.peek() == '\n')
+		std::cin.ignore();
+	
+	std::cout << "Enter your last name: ";
+	
+	std::cin >> last_name;
+	
+	return;
+}
+//prints full name
+void Name::get_full_name() const
+{
+	std::cout << last_name << "," << first_name << std::endl;
+	return;
+}
+
 // returns formatted data for output to console
 std::string Name::to_string() const
 {
@@ -25,4 +48,12 @@ std::string Name::to_string() const
 	return output_string;
 }
 
+//retruns formatted data for output to file
+std::string Name::to_file() const
+{
+	std::string output_string;
 
+	output_string = last_name + ", " + first_name;
+
+	return output_string;
+}
--- Name.h
+++ Name.h
@@ -10,8 +10,9 @@ public:
 	Name(); // default constructor
 	Name(std::string fname, std::string lname); // secondary constructor with arguments
 	void set_full_name(); // set both data members
-	std::string to_string() const; // output both data members
-	std::string to_file() const;
+	void get_full_name() const; //prints both data members
+	std::string to_string() const; // output both data members as a string
+	std::string to_file() const;	//				"
 private:
 	std::string first_name;
 	std::string last_name;
