d3d6211d1199da5b2162ca23c24d682ca6e942ba
happyklans <bstanton4@student.cccs.edu>
2017-09-20 12:30:09 -0600

    added file io to populate the address_book

--- AddrTest.cpp
+++ AddrTest.cpp
@@ -7,15 +7,25 @@ Menu program for the address book aplication
 
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <thread>
 #include <chrono>
+#include "addr_book.h"
+#include "Address.h"
+#include "Contact.h"
+#include "Name.h"
 using namespace std;
 
 
 int main()
 {
 	int menu_choice = 0; // user entered selection for initiating the correct function 
+	
 	string input_validator; // used to ensure valid input
+
+	fstream address_book("address.csv"); //long term stroage for our address book
+
+	addr_book my_contacts;
 
 	// opening graphic for flourish
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
@@ -31,6 +41,85 @@ int main()
 	std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	cout << endl << endl;
 
+	address_book.open("address.csv", ios::out); //open the csv for reading
+
+	/*
+	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	MOVE TO BE THE READ_FILE METHOD IN THE ADDR_BOOK CLASS ONCE CHANGES ARE COMMITED
+	DO NOT LEAVE IN MAIN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	*/
+
+	if (address_book.eof == false)//does not enter the loop if the file is empty
+	{
+		do
+		{
+			/*
+			illustrating the constructor flow
+
+																	 / first_name_constructor
+															  |---> <
+															  |      \ last_name_constructor
+										  / name_constructor -|
+			[  ]  /						 /  address_constructor-|    / street_address_constructor
+			[  ] <		constructor		<	phone_constructor   |-> <  city_constructor
+			[  ]  \						 \  email_constructor        \ state_constructor
+			...							  \ bday_constructor		   zip_constructor
+											picture_file_constructor
+
+							^					    ^						  ^
+							|					    |					      |
+					primary constructor   secondary constructors      tertiary constructors
+
+
+
+
+
+
+			*/
+			int i = 0; //iterator position
+
+			string first_name_constructor; //tertiary constructor for constructing the secondary constructors
+
+			string last_name_constructor;//				"
+
+			string street_address_constructor; //				"
+
+			string city_constructor;//					"
+
+			string state_constructor;//					"
+
+			string zip_constructor;//					"
+
+			string phone_constructor; //secondary constructor for constructing the primary constructor
+
+			string email_constructor;//						"
+
+			string bday_constructor;//						"
+
+			string picture_file_constructor;//				"
+
+			address_book >> first_name_constructor >> last_name_constructor >> street_address_constructor
+				>> city_constructor >> state_constructor >> zip_constructor >> phone_constructor >> email_constructor
+				>> bday_constructor >> picture_file_constructor;
+
+			Name name_constructor(first_name_constructor, last_name_constructor); //secondary constructor for constructing the primary constructor
+
+			Address address_constructor(street_address_constructor, city_constructor, state_constructor, zip_constructor); //secondary constructor for constructing the 
+			//primary constructor
+
+			Contact constructor(name_constructor, address_constructor, phone_constructor, email_constructor, bday_constructor, picture_file_constructor); //block scope 
+			//constructor variable for populating the my_contacts object
+
+			my_contacts.add_contact(constructor);
+
+		} while (address_book.peek() != EOF);
+	}
+	/*
+	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	*/
+	
+	
+	
 	//actual menu and input validation
 	while (menu_choice != 5)
 	{
