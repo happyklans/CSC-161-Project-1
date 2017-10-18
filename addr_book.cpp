8600ea632e0d06649774f4d3afd325829dcb50ad
happyklans <bstanton4@student.cccs.edu>
2017-10-18 12:57:54 -0600

    actually finished project 3

--- addr_book.cpp
+++ addr_book.cpp
@@ -66,7 +66,7 @@ void Addr_Book::remove_contact(Categorized_contact itemToRemove)
 	{
 		index_to_replace = get_used() - 1;
 		
-		if (itemToRemove.to_string() == address_book[i].to_string())
+		if (itemToRemove == address_book[i])
 		{
 			address_book[i] = address_book[index_to_replace];
 			
@@ -81,27 +81,10 @@ void Addr_Book::remove_contact(Categorized_contact itemToRemove)
 
 int Addr_Book::find_contact(Categorized_contact itemToFind) const
 {
-	bool found = false;
-	
-	Field search_value;
-
-	Field possible_match;
-
-	search_value = itemToFind.to_string();
-
-	
 	
 	for (int i = 0; i < get_used(); i++)
 	{
-		possible_match = address_book[i].to_string();
-
-		if (search_value == possible_match)
-			found = true;
-		else
-			found = false;
-		
-
-		if (found == true)
+		if (itemToFind == address_book[i])
 			return i;
 	}
 
@@ -127,7 +110,7 @@ void Addr_Book::print_all_contacts() const
 {
 	for (int i = 0; i < get_used(); i++)
 	{
-		std::cout << i+1 << ")\n" << address_book[i].to_string() << std::endl;
+		std::cout << i+1 << ")\n" << address_book[i] << std::endl;
 	}
 
 	return;
@@ -138,7 +121,7 @@ void Addr_Book::print_by_category(Field category) const
 	for (int i = 0; i < get_used(); i++)
 	{
 		if (address_book[i].get_category() == category)
-			std::cout << i + 1 << ")\n" << address_book[i].to_string() << std::endl;
+			std::cout << i + 1 << ")\n" << address_book[i] << std::endl;
 		else continue;
 	}
 
@@ -151,7 +134,7 @@ int Addr_Book::get_used() const
 
 	for (int i = 0; i < capacity; i++)
 	{
-		if (null_contact.to_string() == address_book[i].to_string())
+		if (null_contact == address_book[i])
 			return i;
 
 	}
@@ -175,106 +158,12 @@ void Addr_Book::read_file(Field filename)
 	}
 
 
-	while (std::getline(address_file, line))
+	while (!address_file.fail())
 	{
-		
-			Field category_constructor; // primary constructor 
 
-			Field first_name_constructor; //tertiary constructor for constructing the secondary constructors
+			Categorized_contact contact_to_add; // element to be added
 
-			Field last_name_constructor;//				"
-
-			Field street_address_constructor; //				"
-
-			Field city_constructor;//					"
-
-			Field state_constructor;//					"
-
-			Field zip_constructor;//					"
-
-			Field phone_constructor; //secondary constructor for constructing the primary constructor
-
-			Field email_constructor;//						"
-
-			Field bday_constructor;//						"
-
-			Field picture_file_constructor;//				"
-
-			
-
-			int field_tracker = 0; //tracks commas to determine the field being inputed
-
-			for (char i : line)
-				{
-					if (i != ',' && i != '\n')
-					{
-						switch (field_tracker)
-						{
-						case 0:
-							category_constructor += i;
-
-							break;
-
-						case 1:
-							first_name_constructor += i;
-
-							break;
-						case 2:
-							last_name_constructor += i;
-
-							break;
-						case 3:
-							street_address_constructor += i;
-
-							break;
-						case 4:
-							city_constructor += i;
-
-							break;
-						case 5:
-							state_constructor += i;
-
-							break;
-						case 6:
-							zip_constructor += i;
-
-							break;
-						case 7:
-							phone_constructor += i;
-
-							break;
-						case 8:
-							email_constructor += i;
-
-							break;
-						case 9:
-							bday_constructor += i;
-
-							break;
-						case 10:
-							picture_file_constructor += i;
-
-							break;
-						}
-
-					}
-
-					else
-					{
-						field_tracker++;
-					}
-				}
-			
-			Name name_constructor(first_name_constructor,
-				last_name_constructor); //secondary constructor for constructing the primary constructor
-
-			Address address_constructor(street_address_constructor,
-				city_constructor, state_constructor, zip_constructor); // "
-
-			Contact contact_constructor(name_constructor, address_constructor, phone_constructor,
-				email_constructor, bday_constructor, picture_file_constructor); // primary constructor
-
-			Categorized_contact contact_to_add(category_constructor, contact_constructor); // element to be added
+			address_file >> contact_to_add;
 
 			add_contact(contact_to_add);
 
@@ -293,7 +182,7 @@ void Addr_Book::write_file(Field filename) const
 
 	for (int i = 0; i < get_used(); i++)
 	{
-		out_file << address_book[i].to_file_string();
+		out_file << address_book[i];
 	}
 
 	out_file.close();
